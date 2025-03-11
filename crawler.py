6dae048561d29c5ba0e65220fa7c9e304a3a2b52
BryanRO-SpTech <bryan.grocha@sptech.school>
2025-03-11 18:28:02 -0300

    Crawler v2

---
+++ crawler.py
@@ -0,0 +1,182 @@
+import HardwareData
+import time
+from dbConnection import cursor as mysql, connection
+
+def company_data():
+    print("\n 🔑 Antes de continuar, precisamos validar sua identidade...")
+
+    while True:
+        idEmpresa = input("Insira o id da sua empresa, o ID pode ser visualizado no nosso site. Digite aqui: ")
+
+        if not idEmpresa.isdecimal():
+            print("O ID é númerico.")
+
+        else:
+            return idEmpresa
+
+
+
+def init():
+    print("Iniciando verificação de Hardware... \n")
+
+    system_info = HardwareData.SystemData()
+    cpu_info = HardwareData.CPUData()
+    ram_info = HardwareData.RAMData()
+    gpu_info = HardwareData.GPUData()
+
+    print(f"⚙️ Sistema operacional: {system_info}")
+    print(f"🔑 UUID da placa mãe: {system_info.motherboardUuid}")
+    print(f"🧠 Núcleos do processador: {cpu_info.cores}")
+    print(f"⚙️ Threads do processador: {cpu_info.threads}")
+    print(f"💾 Memória instalada: {ram_info.total}Gb")
+    print(f"🔄 Memória Swap: {ram_info.totalSwap}Gb")
+
+    for gpu in gpu_info.gpus:
+        print(f"🖥️ Placa de vídeo: {gpu.name}")
+
+
+    # Validando identidade do usuário:
+    idEmpresa = company_data()
+
+    # Verificando servidor no banco de dados:
+    print("\n⏳ Comparando informações com o banco de dados...")
+
+    database_server_verify(system_info, cpu_info, ram_info, idEmpresa)
+    time.sleep(2)
+    database_gpu_verify(gpu_info, system_info)
+    time.sleep(1)
+
+
+    # Menu de pções para o usuário:
+    print("🔧 Menu de Ações:")
+    print("✏️  Digite a opção desejada para continuar:")
+    print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+    print("1  Iniciar monitoramento")
+    print("2  Sair")
+    print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+
+    while True:
+        opt = input("Escolha uma opção: ")
+
+        if not opt.isdecimal():
+            print("A opção deve ser um numero")
+        elif int(opt) == 1:
+            monitoring(system_info, cpu_info, ram_info, gpu_info)
+            break
+        elif int(opt) == 2:
+            exit("Obrigado!")
+
+
+
+def database_server_verify(
+        system_info: HardwareData.SystemData,
+        cpu_info: HardwareData.CPUData,
+        ram_info: HardwareData.RAMData,
+        company: int
+):
+    mysql.execute("SELECT * FROM Server WHERE uuidMotherboard = %s", (system_info.motherboardUuid,))
+    verify_motherboard_uuid = mysql.fetchone()
+
+    if verify_motherboard_uuid:
+        uuid, cores, threads, ram, so, version = verify_motherboard_uuid[:6]
+
+        if cores != cpu_info.cores or threads != cpu_info.threads or ram != ram_info.total or so != system_info.SO\
+                or version != system_info.version:
+
+            mysql.execute("UPDATE Server SET cpuCores = %s, cpuThreads = %s, RAM = %s, SO = %s, version = %s",
+                          (cpu_info.cores, cpu_info.threads, ram_info.total, system_info.SO, system_info.version))
+            connection.commit()
+
+            print("\n🆕 Hardware novo detectado. A base de dados foi atualizada.")
+
+        print("\n✅ Servidor existente no banco de dados e validado com sucesso.")
+
+    else:
+        mysql.execute("INSERT INTO Server VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                      (system_info.motherboardUuid, cpu_info.cores, cpu_info.threads, ram_info.total, system_info.SO,
+                       system_info.version, company))
+        connection.commit()
+
+        print("\n✅ Servidor novo registrado com sucesso...")
+
+
+
+
+
+
+
+def database_gpu_verify(
+        gpu_info: HardwareData.GPUData,
+        system_info: HardwareData.SystemData
+):
+    # Verificando placas de vídeo
+    for gpu in gpu_info.gpus:
+        mysql.execute("SELECT * FROM GPU WHERE uuid = %s", (gpu.uuid,))
+        result = mysql.fetchone()
+
+        if result:
+            if result[3] != system_info.motherboardUuid:
+                mysql.execute("UPDATE GPU SET fkServer = %s WHERE uuid = %s", (system_info.motherboardUuid, gpu.uuid))
+                print(f"⚠️ Placa de vídeo {gpu.name} transferida de servidor. \n")
+
+            print(f"✅ Placa de video {gpu.name} verificada e operante. \n")
+
+        else:
+            mysql.execute("INSERT INTO GPU VALUES (%s, %s, %s, %s)",
+                              (gpu.uuid, gpu.name, gpu.memoryTotal, system_info.motherboardUuid))
+            print(f"✅ Placa de video {gpu.name} cadastrada com sucesso. \n")
+
+        connection.commit()
+
+
+
+
+
+
+
+def monitoring(
+        system_info: HardwareData.SystemData,
+        cpu_info: HardwareData.CPUData,
+        ram_info: HardwareData.RAMData,
+        gpu_info: HardwareData.GPUData
+):
+    print("\n⏳ \033[1;34mCapturando informações de hardware... \033[0m\n"
+          "🛑 Pressione \033[1;31mESC\033[0m para encerrar a captura.")
+
+    def insert_server_log():
+        mysql.execute("INSERT INTO ServerMonitoring (cpuLoad, ramUsed, clock, fkServer) VALUES (%s, %s, %s, %s)", (
+            cpu_info.use, ram_info.used, cpu_info.freq, system_info.motherboardUuid
+        ))
+        connection.commit()
+
+    def insert_gpu_log():
+        for gpu in gpu_info.gpus:
+            mysql.execute("INSERT INTO GPUMonitoring (GPUload, vramUSed, temperature, fkGPU) VALUES (%s, %s, %s, %s)", (
+                round(gpu.load * 100, 2), gpu.memoryUsed, gpu.temperature, gpu.uuid
+            ))
+
+        connection.commit()
+
+
+    while True:
+        cpu_info.update()
+        ram_info.update()
+        gpu_info.update()
+
+        insert_server_log()
+        insert_gpu_log()
+
+        # Formatação ANSI: \33[: escape ANSI, 1 = negrito; 99 = cor; m = Finaliza o ANSI
+
+        for _ in range(200):
+            if keyboard.is_pressed("esc"):
+                print("\n🛑 \033[1;31mInterrompendo a captura de dados... 🔄")
+                print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+                print("⚠️  \033[1;33mA captura foi interrompida com sucesso.")
+                exit(0)
+
+            time.sleep(0.01)
+
+
+
+init()
