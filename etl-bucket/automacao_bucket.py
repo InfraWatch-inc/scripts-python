aece14bad3f48d28af2eecda757ce489c27495e9
RanierDalton <ranier.couto@sptech.school>
2025-04-22 13:44:12 -0300

    Estrutura do script de envio de arquivos para o bucket

---
+++ etl-bucket/automacao_bucket.py
@@ -0,0 +1,23 @@
+import json
+import requests
+import tempfile
+import os
+import boto3
+import time
+
+# rodar script
+# entrar loop
+# time.sleep de 24 horas
+# a cada 24 horas vai selecionar os dados do banco
+    # vai organizar para ficar json colunar por captura
+    # guardar a data Hora mais recente para ignorar as informações antigas
+# vai enviar para o b
+
+def enviar_arquivo():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
